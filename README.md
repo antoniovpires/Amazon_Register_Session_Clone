f985b510424208195990c512cdfdffc22e4e19cb
unknown <antoniovpiresnt@gmail.com>
2022-07-14 14:02:08 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Amazon_Register_Clone
+ A clone I've developed to learn PHP and practice my "registrantion" skills.
---
+++ registration.php
@@ -0,0 +1,47 @@
+<?php 
+    require_once('config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <?php 
+        if(isset($_POST['create'])){
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $sql = "INSERT INTO users (firstname, lastname, email)"
+        }
+        ?>
+    </div>
+    <header>
+        <img id="amazon_logo" src="assets/amazon-com-br-logo.png" alt="amazon logo">
+    </header>
+    <div class="register_div">
+        <form action="registration.php" method="post">
+            <h1>Criar conta</h1>
+            <h4>Seu nome</h4>
+            <input type="text" placeholder="Nome e sobrenome" name="name" required>
+            <h4>Seu e-mail</h4>
+            <input type="email" name="email" required>
+            <h4>Senha</h4>
+            <input type="password" placeholder="Pelo menos 6 caracteres" name="password" required>
+            <h5>As senhas devem ter pelo menos 6 caracteres.</h5>
+            <h4>Insira a senha nova mais uma vez</h4>
+            <input type="password" required>
+            <br>
+            <button type="submit" name="create">Continuar</button>
+        </form>
+        <p class="terms">Ao criar uma conta, você concorda com as <a href="">Condições de Uso</a> da Amazon. Por favor verifique a <a href="">Notificação de Privacidade</a>, <a href="">Notificação de Cookies</a> e a <a href="">Notificação de Anúncios Baseados em Interesse</a>.</p>
+        <p class="if_login">Você já tem uma conta? <a href="">Fazer login</a></p>
+    </div>
+</body>
+</html>
