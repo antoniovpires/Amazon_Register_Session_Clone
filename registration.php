76cfed045432e071907599238fe1fcdfcf52be19
unknown <antoniovpiresnt@gmail.com>
2022-07-14 19:46:07 -0300

    update 0.2
    testesteste

--- registration.php
+++ registration.php
@@ -11,45 +11,106 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div>
-        <?php 
-        if(isset($_POST['create'])){
-            $name = $_POST['username'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-
-            $sql = "INSERT INTO users (name, email, password) VALUES(?,?,?)";
-            $stmtinsert = $db->prepare($sql);
-            $result = $stmtinsert->execute
-            ([$name, $email, $password]);
-            if($result){
-                echo 'Successfully saved.';
-            } else {
-                echo 'There were errors while saving the data.';
-            }
-        }
-        ?>
-    </div>
     <header>
         <img id="amazon_logo" src="assets/amazon-com-br-logo.png" alt="amazon logo">
     </header>
     <div class="register_div">
-        <form action="registration.php" method="post">
+        <form action="registration.php" id="theForm" method="post">
             <h1>Criar conta</h1>
             <h4>Seu nome</h4>
-            <input type="text" placeholder="Nome e sobrenome" name="username" required>
+            <input type="text" id="name" placeholder="Nome e sobrenome" name="username" required>
             <h4>Seu e-mail</h4>
-            <input type="email" name="email" required>
+            <input id="email" type="email" name="email" required>
             <h4>Senha</h4>
-            <input type="password" placeholder="Pelo menos 6 caracteres" name="password" required>
+            <input id="password" type="password" placeholder="Pelo menos 6 caracteres" name="password" required>
             <h5>As senhas devem ter pelo menos 6 caracteres.</h5>
-            <h4>Insira a senha nova mais uma vez</h4>
-            <input type="password" required>
+            <h4>Insira a senha mais uma vez</h4>
+            <input type="password" id="confirm_password" required>
             <br>
-            <button type="submit" name="create">Continuar</button>
+            <button id="register" type="submit" name="create">Continuar</button>
         </form>
-        <p class="terms">Ao criar uma conta, você concorda com as <a href="">Condições de Uso</a> da Amazon. Por favor verifique a <a href="">Notificação de Privacidade</a>, <a href="">Notificação de Cookies</a> e a <a href="">Notificação de Anúncios Baseados em Interesse</a>.</p>
-        <p class="if_login">Você já tem uma conta? <a href="">Fazer login</a></p>
+        <p class="terms">Ao criar uma conta, você concorda com as <a href="" class="a_link">Condições de Uso</a> da Amazon. Por favor verifique a <a href="" class="a_link">Notificação de Privacidade</a>, <a href="" class="a_link">Notificação de Cookies</a> e a <a href="" class="a_link">Notificação de Anúncios Baseados em Interesse</a>.</p>
+        <p class="if_login">Você já tem uma conta? <a href="" class="a_link">Fazer login</a></p>
     </div>
+    <div class="links_cc">
+        <div class="links_div">
+            <div class="links_subdiv">
+                <h6><a href="#" class="a_link">Condições de Uso</a></h6>
+                <h6><a href="#" class="a_link">Política de Privacidade</a></h6>
+                <h6><a href="#" class="a_link">Ajuda</a></h6>
+                <h6><a href="#"class="a_link">Cookies</a></h6>
+                <h6><a href="#" class="a_link">Anúncios Baseados em Interesses</a></h6>
+            </div>
+        </div>
+        <div class="cc">
+            <h6>© 2021-2022 Amazon.com, Inc. ou suas afiliadas</h6>
+        </div>
+    </div>
+
+    <!--SCRIPT SECTION -->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script>
+        $(function(){
+            $('#register').click(function(e){
+
+                var valid = this.form.checkValidity();
+                var password = $('#password').val();
+                var confirm_password = $('#confirm_password').val();
+                var re = /^.{6,}$/
+
+                if (re.test(password)) {
+                    if (password === confirm_password) {
+                        if (valid){
+                            var username = $('#name').val();
+                            var email = $('#email').val();
+                            
+                            e.preventDefault();    
+
+                            $.ajax({
+                                type: 'POST',
+                                url: 'process.php',
+                                data:{username: username, email: email, password: password},
+                                success: function(data){
+                                    Swal.fire({
+                                        'title': 'Successful',
+                                        'text': data,
+                                        'icon': 'success',
+                                        confirmButtonText: 'Awesome!'
+                                    });
+                                },
+                                error: function(data) {
+                                    Swal.fire({
+                                        'title': 'Error',
+                                        'text': 'There were errors while saving the data.',
+                                        'icon': 'error',
+                                        confirmButtonText: 'Try Again'
+                                    });
+                                }
+                            });
+                        } else {
+                            alert('false');
+                        }
+                    } else {
+                        e.preventDefault();
+                        Swal.fire({
+                            'title': 'Erro',
+                            'text': 'Você inseriu duas senhas diferentes.',
+                            'icon': 'error',
+                            confirmButtonText: 'Ok'
+                        });
+                    }
+                } else {
+                    e.preventDefault();
+                    Swal.fire({
+                        'title': 'Erro',
+                        'text': 'Sua senha deve ter no mínimo 6 caracteres.',
+                        'icon': 'error',
+                        confirmButtonText: 'Ok'
+                    });
+                }
+            })
+        });
+    </script>
 </body>
 </html>
